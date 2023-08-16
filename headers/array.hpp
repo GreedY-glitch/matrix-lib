198d272161aea7c92d8a3fc3472dad14f35eba23
GreedY-glitch <dnspopagat@gmail.com>
2023-08-17 03:58:01 +0800

    Added initialization_list

--- headers/array.hpp
+++ headers/array.hpp
@@ -5,13 +5,16 @@
 #define __ARRAY
 
 
+#include <iostream>
+#include <initializer_list>
+
+
 typedef unsigned int uint32_t;
 
 template <class T, uint32_t N> class Array {
 private:
     using value_type = T;
     using size_type = uint32_t;
-    using difference_type = std::ptrdiff_t;
     using reference = value_type&;
     using const_reference = const value_type&;
     using pointer = value_type*;
@@ -29,8 +32,30 @@ public:
     }
 
     Array(pointer _arr) {
-        
+        for (int i = 0; i < N; i++) {
+            __array[i] = _arr[i];
+        }
     }
+
+    Array(const std::initializer_list<T>& il) {
+        typename std::initializer_list<T>::iterator iter = il.begin();
+
+        uint32_t i = 0;
+        for (; iter != il.end(); ++iter, i++) {
+            __array[i] = *iter;
+        }
+    } 
+
+    Array<T, N> operator=(pointer) noexcept;
 };
+
+
+
+template <class T, uint32_t N>
+inline Array<T, N> Array<T, N>::operator=(pointer _arr) noexcept
+{
+    for (int i = 0; i < N; i++) __array[i] = _arr[i];
+    return this;
+}
 
 #endif // __ARRAY
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include </home/greed/Desktop/Matrix library/headers/array.hpp>
+
+
 int main() {
+    Array<int, 1> arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
     return 0;
 }
